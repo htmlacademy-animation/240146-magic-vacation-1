57338fc573934692b125e341995117bcb987c09c
mindescape <escape.outerspace@gmail.com>
2020-11-09 20:49:00 +0300

    Показывать анимацию приза только один раз

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,6 +1,20 @@
 export default () => {
+  const prizesScreen = document.querySelector(`.screen--prizes`);
+  const primaryAwardImg = document.querySelector(`.prizes__item-svg`);
+  const primaryAwardSvgPath = `img/primary-award.svg`;
+  let animationShown = false;
+
+  document.body.addEventListener(`screenChanged`, () => {
+    const prizesScreenActive = prizesScreen.classList.contains(`active`);
+
+    if (prizesScreenActive && !animationShown) {
+      primaryAwardImg.src = `${primaryAwardSvgPath}?${Math.random()}`;
+      animationShown = true;
+    }
+  });
+
+
   // const screenOverlay = document.querySelector(`.screen-overlay`);
-  // const prizesScreen = document.querySelector(`.screen--prizes`);
 
   // document.body.addEventListener(`screenChanged`, () => {
   //   const prizesScreenActive = prizesScreen.classList.contains(`active`);
