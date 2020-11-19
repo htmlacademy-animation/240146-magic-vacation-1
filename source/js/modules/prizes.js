d6e49730ae02cfac443776c4d242f4f72f9e1881
mindescape <escape.outerspace@gmail.com>
2020-11-19 15:14:50 +0300

    Добавление таймингов появления svg анимаций на странице призов

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,7 +1,15 @@
 export default () => {
   const prizesScreen = document.querySelector(`.screen--prizes`);
-  const primaryAwardImg = document.querySelector(`.prizes__item-svg`);
+
+  const primaryAwardImg = document.querySelector(`.prizes__item-primary-svg`);
   const primaryAwardSvgPath = `img/primary-award.svg`;
+
+  const secondaryAwardImg = document.querySelector(`.prizes__item-secondary-svg`);
+  const secondaryAwardSvgPath = `img/secondary-award.svg`;
+
+  const additionalAwardImg = document.querySelector(`.prizes__item-additional-svg`);
+  const additionalAwardSvgPath = `img/additional-award.svg`;
+
   let animationShown = false;
 
   document.body.addEventListener(`screenChanged`, () => {
@@ -9,10 +17,21 @@ export default () => {
 
     if (prizesScreenActive && !animationShown) {
       primaryAwardImg.src = `${primaryAwardSvgPath}?${Math.random()}`;
+
+      // 0.3s + 0.05 + 0.2s + 0.3s + 1s = 2000ms
+
+      setTimeout(() => {
+        secondaryAwardImg.src = `${secondaryAwardSvgPath}?${Math.random()}`;
+      }, 3900);
+
+
+      setTimeout(() => {
+        additionalAwardImg.src = `${additionalAwardSvgPath}?${Math.random()}`;
+      }, 5900);
+
       animationShown = true;
     }
   });
-
 
   // const screenOverlay = document.querySelector(`.screen-overlay`);
 
