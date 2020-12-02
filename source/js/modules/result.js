fc6409ffe29bdd92d6d68373d33abb3195171030
mindescape <escape.outerspace@gmail.com>
2020-12-02 19:35:18 +0300

    Реализовываем анимацию заголовков на экране «Результат игры»

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -1,30 +1,29 @@
 export default () => {
-  // const paths = document.querySelectorAll(`.result path`);
+  const paths = document.querySelectorAll(`.result path`);
 
-  // paths.forEach((path) => {
-  //   const dashArray = path.getTotalLength();
-  //   const animate = document.createElementNS(`http://www.w3.org/2000/svg`, `animate`);
+  paths.forEach((path) => {
+    const dashArray = path.getTotalLength();
+    const animate = document.createElementNS(`http://www.w3.org/2000/svg`, `animate`);
 
-  //   path.setAttribute(`stroke-dasharray`, Math.floor(dashArray / 3));
-  //   animate.setAttribute(`attributeName`, `stroke-dasharray`);
-  //   animate.setAttribute(`from`, `0 ` + Math.floor(dashArray / 3));
-  //   animate.setAttribute(`to`, Math.floor(dashArray / 3) + ` 0`);
-  //   animate.setAttribute(`dur`, `1s`);
-  //   animate.setAttribute(`begin`, `click`);
-  //   animate.setAttribute(`fill`, `freeze`);
-  //   animate.setAttribute(`keyTimes`, `0; 1`);
-  //   animate.setAttribute(`keySplines`, `0.21, 0, 0.8, 1`);
+    path.setAttribute(`stroke-dasharray`, Math.ceil(dashArray / 3));
+    animate.setAttribute(`attributeName`, `stroke-dasharray`);
+    animate.setAttribute(`from`, `0 ` + Math.ceil(dashArray / 3));
+    animate.setAttribute(`to`, Math.ceil(dashArray / 3) + ` 0`);
+    animate.setAttribute(`dur`, `0.5s`);
+    animate.setAttribute(`begin`, `startDashAnimation`);
+    animate.setAttribute(`fill`, `freeze`);
+    animate.setAttribute(`keyTimes`, `0; 1`);
+    animate.setAttribute(`keySplines`, `0.21, 0, 0.8, 1`);
 
-  //   path.appendChild(animate);
-  // });
+    path.appendChild(animate);
+  });
 
-  // const paths2 = document.querySelectorAll(`.result__lose path`);
-  // let delay = 0;
-
-  // paths2.forEach((path) => {
-  //   path.style.animationDelay = `${delay}s`;
-  //   delay += 0.08;
-  // });
+  const pathsLose = document.querySelectorAll(`.result__lose path`);
+  let timeOffset = 0;
+  pathsLose.forEach((path) => {
+    path.style.animationDelay = `${timeOffset}s`;
+    timeOffset += 0.05;
+  });
 
   let showResultEls = document.querySelectorAll(`.js-show-result`);
   let results = document.querySelectorAll(`.screen--result`);
@@ -40,17 +39,14 @@ export default () => {
           return el.getAttribute(`id`) === target;
         });
         targetEl[0].classList.add(`screen--show`);
-
-
-        // const pathsResult = document.querySelectorAll(`.screen--show .result path`);
-
-        // setTimeout(() => {
-        //   pathsResult.forEach((path) => {
-        //     path.dispatchEvent(new Event(`click`));
-        //   });
-        // }, 10);
-
         targetEl[0].classList.remove(`screen--hidden`);
+
+        const pathsResult = document.querySelectorAll(`.screen--show .result path`);
+        setTimeout(() => {
+          pathsResult.forEach((path) => {
+            path.dispatchEvent(new Event(`startDashAnimation`));
+          });
+        }, 10);
 
       });
     }
